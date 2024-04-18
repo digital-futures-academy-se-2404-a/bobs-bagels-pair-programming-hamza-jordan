c57ac0354a7cb071d20fa53115dbfac2a10ee612
Mohammad Hamza Ibrahim <mi469@live.mdx.ac.uk>
2024-04-18 18:23:42 +0100

    new file setup and test 1 completed (Add item to basket and increased in length by 1)

---
+++ test_cases/src/basket.js
@@ -0,0 +1,7 @@
+export const basket = {
+  addBagel: function (bagel) {
+        this.items.push(bagel);
+},
+    items: [],
+};
+
---
+++ test_cases/src/index.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* Production code file
+
+// export const addBagel = (bagelName,basketArray) => {
+//   // const basketArray = [];
+//   basketArray.push(bagelName)
+//   return basketArray[basketArray.length - 1];
+// };
+
+// export const removeBagel = (bagelName,basketArray) => {
+//   // const basketArray = [];
+  
+//   const index = basketArray.indexOf(bagelName);
+//   const removedBagel = basketArray.splice(index, 1).toString();
+//   return removedBagel;
+// };
